4336e1eae75de35c726d32fe57c9e9713806f892
Simen Kvalø <simekva@stud.ntnu.no>
2024-03-12 12:21:48 +0100

    Setup GDX.

---
+++ main/desktop/src/com/gdx/jigsawgenius/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.gdx.jigsawgenius;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.gdx.jigsawgenius.main;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Jigsaw Genius");
+		new Lwjgl3Application(new main(), config);
+	}
+}
