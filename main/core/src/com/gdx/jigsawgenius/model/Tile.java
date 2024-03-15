2bd2e2bad72018e49736f43c70c38942ddc9ed76
haakoaal <haakoaal@stud.ntnu.no>
2024-03-15 17:06:02 +0100

    feat: added suggestions for Player and Tilemanager classes

--- main/core/src/com/gdx/jigsawgenius/model/Tile.java
+++ main/core/src/com/gdx/jigsawgenius/model/Tile.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 public class Tile {
     private List<Side> sides = new LinkedList<Side>();
+    static final int sidesCount = 6;
 
     public Tile(List<Side> list) {
         // Tile has to have a size of 6
-        if (list.size() != 6) {
+        if (list.size() != sidesCount) {
             throw new IllegalArgumentException("Tile has to have 6 sides");
         }
         this.sides = list;
@@ -29,10 +30,10 @@ public class Tile {
         tempSides.add(sides.get(4));
         this.sides = tempSides;
     }
-    
+
     @Override
     public String toString() {
-        String returnString = "[";
+        String returnString = "Tile: [";
         for (int i = 0; i < this.sides.size(); i++) {
             returnString += sides.get(i).getTerrainType();
             if (i != this.sides.size() - 1) {
