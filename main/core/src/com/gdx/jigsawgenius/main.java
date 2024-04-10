dee88faef0726af620f799ba5ddd667ec4b16d81
Simen Kvalø <simekva@stud.ntnu.no>
2024-04-10 07:14:46 +0200

    Implemented camera

--- main/core/src/com/gdx/jigsawgenius/main.java
+++ main/core/src/com/gdx/jigsawgenius/main.java
@@ -11,7 +11,9 @@ import com.gdx.jigsawgenius.view.AdjacentTileDrawer;
 import com.gdx.jigsawgenius.view.TileDrawer;
 
 public class main extends ApplicationAdapter {
-	// private Assets assets;
+
+	static final int WORLD_WIDTH = 10000;
+	static final int WORLD_HEIGHT = 10000;
 	Assets assets;
 	PolygonSpriteBatch batch;
 	OrthographicCamera camera;
@@ -22,25 +24,29 @@ public class main extends ApplicationAdapter {
 
 	@Override
 	public void create() {
+
 		assets = new Assets();
 		batch = new PolygonSpriteBatch();
 
 		camera = new OrthographicCamera(30, 30 * (Gdx.graphics.getHeight() / Gdx.graphics.getWidth()));
 		camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
+		camera.zoom = 25;
 		camera.update();
 
 		controller = new GameLogicController();
-		controller.placeTile(2, 0);
-		controller.placeTile(-2, 0);
 		drawer = new AdjacentTileDrawer(controller.getBoard().getTile(0, 0), controller.getBoard());
+		System.out.println(camera.position.x);
 	}
 
 	@Override
 	public void render() {
 		assets.manager.finishLoading();
+		handleCamera();
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		drawer.drawAdjacentTiles(assets, batch, 500, 500);
+		drawer.drawAdjacentTiles(assets, batch, 0, 0);
 		batch.end();
 		assets.manager.update();
 	}
@@ -49,5 +55,45 @@ public class main extends ApplicationAdapter {
 	public void dispose() {
 		batch.dispose();
 		assets.manager.clear();
+	}
+
+	private void handleCamera() {
+		if (Gdx.input.isKeyJustPressed(com.badlogic.gdx.Input.Keys.A)) {
+			camera.zoom += 10;
+			System.out.println("zooming");
+		}
+		if (Gdx.input.isKeyJustPressed(com.badlogic.gdx.Input.Keys.Q)) {
+			camera.zoom -= 10;
+		}
+		if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.LEFT)) {
+			camera.translate(-5, 0, 0);
+			System.out.println(camera.position.x);
+		}
+		if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.RIGHT)) {
+
+			camera.translate(5, 0, 0);
+			System.out.println(camera.position.x);
+		}
+		if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.DOWN)) {
+			camera.translate(0, -5, 0);
+		}
+		if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.UP)) {
+			camera.translate(0, 5, 0);
+		}
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		camera.viewportWidth = 30f;
+		camera.viewportHeight = 30f * height / width;
+		camera.update();
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void pause() {
 	}
 }
