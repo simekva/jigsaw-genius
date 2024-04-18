abee76828d8959618824364f9acd1a77149ad77e
@eskilgj <eskilgj@stud.ntnu.no>
2024-04-18 20:54:33 +0200

    Can now Update tiles in real time

--- main/core/src/com/gdx/jigsawgenius/view/pinMenu.java
+++ main/core/src/com/gdx/jigsawgenius/view/pinMenu.java
@@ -9,23 +9,25 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.gdx.jigsawgenius.JigsawGenius;
 import com.gdx.jigsawgenius.model.Assets;
-
-
 import com.gdx.jigsawgenius.firebase.FirebaseHost;
-import com.gdx.jigsawgenius.firebase.FirebaseJoin;
-import com.gdx.jigsawgenius.firebase.FirebaseReader;
 
-public class MainMenuScreen extends ScreenAdapter {
+        import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
+
+public class pinMenu extends ScreenAdapter {
 
     private Assets assets;
-
     private final JigsawGenius game;
     private SpriteBatch batch;
     private Texture backgroundTexture;
@@ -33,9 +35,17 @@ public class MainMenuScreen extends ScreenAdapter {
     private Stage stage;
     private Skin skin;
     private BitmapFont font;
+    private TextField codeField;
 
-    public MainMenuScreen(JigsawGenius game) {
+    private String pin;
+    private boolean isHost;
+
+    public pinMenu(JigsawGenius game, String pin, boolean isHost) {
         this.game = game;
+        
+        this.pin = pin;
+
+        this.isHost = isHost;
     }
 
     @Override
@@ -46,7 +56,6 @@ public class MainMenuScreen extends ScreenAdapter {
 
         backgroundTexture = new Texture("background.png");
         ribbonTexture = new Texture("ribbon.png");
-        // Scale down the ribbon image
         ribbonTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
         ribbonTexture.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
 
@@ -64,47 +73,51 @@ public class MainMenuScreen extends ScreenAdapter {
         buttonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
         buttonStyle.font = skin.getFont("default-font");
 
-        TextButton singlePlayerButton = new TextButton("Single Player", buttonStyle);
-        singlePlayerButton.setSize(200, 50);
-        singlePlayerButton.setPosition(Gdx.graphics.getWidth() / 2 - singlePlayerButton.getWidth() / 2,
-                Gdx.graphics.getHeight() / 2);
-        singlePlayerButton.addListener(new ClickListener() {
+        TextButton hostButton = new TextButton("Start game", buttonStyle);
+        hostButton.setSize(200, 50);
+        hostButton.setPosition(Gdx.graphics.getWidth() / 2 - hostButton.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+        hostButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new SinglePlayerScreen(assets, game));
-
+                boolean isHost = true; // For example, you can set it based on some condition
+        
+                // Start the SinglePlayerScreen and pass the pin and isHost variables
+                game.setScreen(new MultiPlayer(assets, game, pin, isHost));
             }
         });
 
-        TextButton multiPlayerButton = new TextButton("Multi Player", buttonStyle);
-        multiPlayerButton.setSize(200, 50);
-        multiPlayerButton.setPosition(Gdx.graphics.getWidth() / 2 - multiPlayerButton.getWidth() / 2,
-                Gdx.graphics.getHeight() / 2 - 100);
-        multiPlayerButton.addListener(new ClickListener() {
+
+
+        Texture backgroundTexture = new Texture("pinBackground.png");
+        NinePatch backgroundPatch = new NinePatch(backgroundTexture, 12, 12, 12, 12);
+        NinePatchDrawable backgroundDrawable = new NinePatchDrawable(backgroundPatch);
+
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = skin.getFont("default-font");
+        labelStyle.fontColor = Color.BLACK;
+        labelStyle.background = backgroundDrawable; // Set the background
+
+        Label codeLabel = new Label("Tell the joining player to \n enter this 4-digit code: " + pin, labelStyle);
+        codeLabel.setSize(220, 100);
+        codeLabel.setPosition(Gdx.graphics.getWidth() / 2 - codeLabel.getWidth() / 2, Gdx.graphics.getHeight() / 2 - 150);
+        codeLabel.setAlignment(Align.center);
+
+
+        TextButton backButton = new TextButton("Go back", buttonStyle);
+        backButton.setSize(100, 120);
+        backButton.setPosition(50, Gdx.graphics.getHeight() - backButton.getHeight() - 50);
+        backButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new MultiPlayerScreen(game));
-
             }
         });
 
-        //Help screen:
-        Texture helpIconTexture = new Texture("help.png");
-        Image helpIcon = new Image(helpIconTexture);
-        helpIcon.setSize(70, 70); 
+        stage.addActor(backButton);
 
-        helpIcon.setPosition(60, 60);
+        stage.addActor(hostButton);
+        stage.addActor(codeLabel);
 
-        helpIcon.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new helpScreen(game));
-            }
-        });
-
-        stage.addActor(singlePlayerButton);
-        stage.addActor(multiPlayerButton);
-        stage.addActor(helpIcon);
     }
 
     @Override
@@ -114,12 +127,6 @@ public class MainMenuScreen extends ScreenAdapter {
 
         batch.begin();
         batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-        float ribbonWidth = ribbonTexture.getWidth() / 2;
-        float ribbonHeight = ribbonTexture.getHeight() / 2;
-        batch.draw(ribbonTexture, Gdx.graphics.getWidth() / 2 - ribbonWidth / 2, Gdx.graphics.getHeight() - 150,
-                ribbonWidth, ribbonHeight);
-
         batch.end();
 
         stage.act();
@@ -135,12 +142,8 @@ public class MainMenuScreen extends ScreenAdapter {
     public void hide() {
         batch.dispose();
         backgroundTexture.dispose();
-        ribbonTexture.dispose();
         stage.dispose();
         skin.dispose();
         font.dispose();
     }
-
-
-    
 }
